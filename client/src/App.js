d323a3e0ccf602f8eb5123499845715d8f3a19a3
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-12 12:03:15 -0500

    Importación de módulos desarrollados

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Importamos el componente que muestra todos los empleados.
+import CompListEmployees from './employee/listEmployees.js';
+// Importamos el componente que crea un empleado.
+import CompCreateEmployee from './employee/createEmployee.js';
+// Importamos el componente que edita a un empleado.
+import CompEditEmployee from './employee/editEmployee.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<CompListEmployees />} />
+            <Route path='/crear' element={<CompCreateEmployee />} />
+            <Route path='/editar/:id' element={<CompEditEmployee />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
